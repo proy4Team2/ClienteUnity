a8d5ef88780eaa6436e99f880ef5ce415cdc2c42
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2026-01-30 18:27:27 +0100

    Cambios UI
    - Ventana detalles cambia según icono pulsado
    - Creada clase singleton con información de detalles de cada experiencia
    - UI (teóricamente) interactuable en VR

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject UILogin, UIMainMenu, UIDetailsMenu;
+    [SerializeField] TMP_Text DetsTitle, DetsPeople, DetsType, DetsDescript;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,14 +21,30 @@ public class UIManager : MonoBehaviour
         UIMainMenu.SetActive(true);
     }
 
-    public void MainToDetails()
-    {
-        UIDetailsMenu.SetActive(true);
-        UIMainMenu.SetActive(false);
-    }
     public void DetailsToMain()
     {
         UIDetailsMenu.SetActive(false);
         UIMainMenu.SetActive(true);
+    }
+
+    public void clickedButtonInterview()
+    {
+        UIDetailsMenu.SetActive(true);
+        DetailsSetup(DetailsData.Instance.Entrevista());
+        UIMainMenu.SetActive(false);
+    }
+    public void clickedButtonReunion()
+    {
+        UIDetailsMenu.SetActive(true);
+        DetailsSetup(DetailsData.Instance.Reunion());
+        UIMainMenu.SetActive(false);
+    }
+
+    public void DetailsSetup(List<string> data)
+    {
+        DetsTitle.text = data[0];
+        DetsPeople.text = data[1];
+        DetsType.text = data[2];
+        DetsDescript.text = data[3];
     }
 }
