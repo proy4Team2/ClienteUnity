c8dee5bd66b25e4aeb7baddcd1eded76e5b5ff25
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2026-02-06 18:58:52 +0100

    UI menú

--- Assets/Scripts/DetailsData.cs
+++ Assets/Scripts/DetailsData.cs
@@ -13,11 +13,29 @@ public class DetailsData : MonoBehaviour
         _instance = this;
     }
 
+    /*
+     INFO:
+    Cada List<string> contiene una serie de datos de cada experiencia, para ser usados con el menú de detalles. Para cada nueva experiencia se
+    debe crear una nueva List<string> según se especifica aquí.
+    Los campos/datos siguen un orden concreto, siendo:
+
+    private List<string> data[NOMBRE] = new List<string> {
+        "[TITULO_EXPERIENCIA]", 
+        "[Nº_PERSONAS]",    //"1 persona" (1-a-1) o "Varias personas" (Parte de un grupo)
+        "[TIPO]",           //Tipo de conversación: "Formal" (trabajo, etc.) o "Informal" (quedar con amigos, conocidos...)
+        "[DESCRIPCIÓN]"
+        "[NOMBRE_ESCENA]"   // IMPORTANTE que coincida con el nombre de la escena correspondiente, se usará para el teletransporte
+
+    };
+     
+     */
+
     private List<string> dataEntrevista = new List<string> {
         "Entrevista", 
         "1 persona", 
         "Formal", 
-        "Realiza una entrevista laboral desde el punto de vista de un aplicante junior, buscando empezar a entrar al mercado laboral."
+        "Realiza una entrevista laboral desde el punto de vista de un aplicante junior, buscando empezar a entrar al mercado laboral.",
+        "INTERVIEW"
     };
 
     public List<string> Entrevista()
@@ -29,7 +47,8 @@ public class DetailsData : MonoBehaviour
         "Reunión", 
         "Varias personas", 
         "Formal",
-        "Participa en una reunión de trabajo con compañeros."
+        "Participa en una reunión de trabajo con compañeros.",
+        "MEETING"
     };
 
     public List<string> Reunion()
