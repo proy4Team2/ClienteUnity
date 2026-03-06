74d0adaaff3af3a663b74532c50dffc0025b268d
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2026-03-06 17:47:44 +0100

    Resultados JSON
    Creada clase que almacena datos json + funcuión preliminar que rellena instancia de esa clase con datos json y asigna datos a texto correspondiente en la UI

---
+++ Assets/API/ResultsClass.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ResultsClass
+{
+    public int score;
+    public string summary;
+    public string strengths;
+    public string weaknesses;
+    public string pacing_feedback;
+
+    public bool passed;
+    public string decision_rationale;
+    public string star_method_check;
+    public string soft_skills;
+    public string red_flags;
+
+    public string immediate_action;
+    public string long_term_advice;
+}
--- Assets/Scripts/UIManagerResult.cs
+++ Assets/Scripts/UIManagerResult.cs
@@ -4,11 +4,14 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using Unity.XR.CoreUtils;
+using Unity.VisualScripting;
 
 public class UIManagerResult : MonoBehaviour
 {
     [SerializeField] GameObject UIRes1, UIRes2, UIMejora;
     [SerializeField] CameraFadeManager FadeManager;
+    private ResultsClass results;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -54,6 +57,24 @@ public class UIManagerResult : MonoBehaviour
             UIMejora.SetActive(false);
             UIRes2.SetActive(true);
         }
+    }
+
+    public void getJsonContents(string jsonString)
+    {
+        results = JsonUtility.FromJson<ResultsClass>(jsonString);
+        UIRes1.transform.Find("Resumen").gameObject.GetComponent<TMP_Text>().text = "RESUMEN:" + results.summary;
+        UIRes1.transform.Find("PuntosF").gameObject.GetComponent<TMP_Text>().text = "PUNTOS FUERTES\n" + results.strengths;
+        UIRes1.transform.Find("PuntosD").gameObject.GetComponent<TMP_Text>().text = "PUNTOS DÉBILES\n" + results.weaknesses;
+        UIRes1.transform.Find("Puntuación").gameObject.GetComponent<TMP_Text>().text = "PUNTUACIÓN: " + results.score.ToString();
+
+        UIRes2.transform.Find("Veredicto").gameObject.GetComponent<TMP_Text>().text = "VEREDICTO: " + (results.passed ? "Aprobado" : "No aprobado");
+        UIRes2.transform.Find("Justificacion").gameObject.GetComponent<TMP_Text>().text = "JUSTIFICACIÓN: " + results.decision_rationale;
+        UIRes2.transform.Find("Soft Skills").gameObject.GetComponent<TMP_Text>().text = "SOFT SKILLS\n" + results.soft_skills;
+        UIRes2.transform.Find("Red Flags").gameObject.GetComponent<TMP_Text>().text = "RED FLAGS\n" + results.red_flags;
+        UIRes2.transform.Find("STAR").gameObject.GetComponent<TMP_Text>().text = "MÉTODO STAR: " + results.star_method_check;
+
+        UIMejora.transform.Find("CortoPText").gameObject.GetComponent<TMP_Text>().text = results.immediate_action;
+        UIMejora.transform.Find("LargoPtext").gameObject.GetComponent<TMP_Text>().text = results.long_term_advice;
     }
 
     //public void StartExperienceButton()
